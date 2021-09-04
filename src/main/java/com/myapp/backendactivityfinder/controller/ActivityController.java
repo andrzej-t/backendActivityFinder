acf5950135ead6147eeb3c531571f66fd2ffa97b
andrzej-t <andrzej.tyrpa@gmail.com>
2021-09-04 11:33:08 +0200

    Making very first working connection between frontend and backend

--- src/main/java/com/myapp/backendactivityfinder/controller/ActivityController.java
+++ src/main/java/com/myapp/backendactivityfinder/controller/ActivityController.java
@@ -5,21 +5,22 @@ import com.myapp.backendactivityfinder.domain.ActivityDto;
 import com.myapp.backendactivityfinder.mapper.ActivityMapper;
 import com.myapp.backendactivityfinder.service.DbService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/activities")
 @RequiredArgsConstructor
 public class ActivityController {
 
-    private final DbService service;
-    private final ActivityMapper activityMapper;
+    @Autowired
+    private DbService service;
+    @Autowired
+    private ActivityMapper activityMapper;
 
+    @GetMapping
     public List<ActivityDto> getActivities() {
         List<Activity> activityList = service.getAllActivities();
         return activityMapper.mapToActivityDtoList(activityList);
