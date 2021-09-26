01b2ced132c28b895543b59b5d57ab1432dd69fb
andrzej-t <andrzej.tyrpa@gmail.com>
2021-09-26 10:33:20 +0200

    Added new tests 66 percent coverage

--- src/main/java/com/myapp/backendactivityfinder/domain/BoredDto.java
+++ src/main/java/com/myapp/backendactivityfinder/domain/BoredDto.java
@@ -2,12 +2,17 @@ package com.myapp.backendactivityfinder.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties
 public class BoredDto {
 
     @JsonProperty("activity")
     private String activity;
+
 }
---
+++ src/test/java/com/myapp/backendactivityfinder/bored/BoredClientTestSuite.java
@@ -0,0 +1,45 @@
+package com.myapp.backendactivityfinder.bored;
+
+import com.myapp.backendactivityfinder.domain.BoredDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BoredClientTestSuite {
+
+    @InjectMocks
+    private BoredClient boredClient;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private BoredConfig boredConfig;
+
+    @Test
+    public void ShouldFetchActivityFromBored() throws URISyntaxException {
+
+        //Given
+        when(boredConfig.getBoredApiEndpoint()).thenReturn("https://test.com");
+
+        URI url = new URI("https://test.com");
+        BoredDto boredDto = new BoredDto("Test my application");
+        when(restTemplate.getForObject(url, BoredDto.class)).thenReturn(boredDto);
+
+        //When
+        BoredDto fetchedActivity = boredClient.getBored();
+
+        //Then
+        assertEquals("Test my application", fetchedActivity.getActivity());
+    }
+}
