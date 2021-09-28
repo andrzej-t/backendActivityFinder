e4583506cd31338b48472a5ead078ac1cf97e213
andrzej-t <andrzej.tyrpa@gmail.com>
2021-09-28 10:03:29 +0200

    Added some cmmented code. Very first preparing for deployment

--- src/main/java/com/myapp/backendactivityfinder/BackendActivityFinderApplication.java
+++ src/main/java/com/myapp/backendactivityfinder/BackendActivityFinderApplication.java
@@ -2,14 +2,20 @@ package com.myapp.backendactivityfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendActivityFinderApplication extends SpringBootServletInitializer {
-//public class BackendActivityFinderApplication {
+//public class BackendActivityFinderApplication extends SpringBootServletInitializer {
+public class BackendActivityFinderApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(BackendActivityFinderApplication.class, args);
     }
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(BackendActivityFinderApplication.class);
+//    }
 }
